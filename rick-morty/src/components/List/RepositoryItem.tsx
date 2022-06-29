fa7f4371a7a506fa8f4eb32803e4e8deb70e3a73
beasou <beatrizreginasouza1@gmail.com>
2022-06-29 16:06:10 -0300

    recuperando dados da api

--- rick-morty/src/components/List/RepositoryItem.tsx
+++ rick-morty/src/components/List/RepositoryItem.tsx
@@ -1,11 +1,21 @@
 import img from '../../assets/morty-talking.svg'
 
-export function RepositoryItem(){
+interface RepositoryItemProps{
+    repository:{
+        image:string,
+        name: string,
+        status: string,
+        species: string,
+        gender: string,
+    }
+}
+
+export function RepositoryItem(props: RepositoryItemProps){
     return(
         <li>
-            <img src={img} alt="" />
-            <h1>Nome</h1>
-            <p>Descrição</p>
+            <img src={props.repository.image} alt="Imagem" />
+            <h1>{props.repository.name}</h1>
+            <p>{props.repository.species}</p>
         </li>
     )
 }
--- rick-morty/src/components/List/index.tsx
+++ rick-morty/src/components/List/index.tsx
@@ -1,13 +1,33 @@
+import { useState, useEffect} from 'react'
 import { RepositoryItem } from './RepositoryItem'
 import { Container } from './style'
 
+const character = "https://rickandmortyapi.com/api/character"
+
+const repository ={
+    image:"",
+    name: 'blbla',
+    status:"ds",
+    species:"efe",
+    gender: "ef",
+
+}
+
 export function List(){
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch(character)
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
     return (
         <Container>
             <ul>
-                <RepositoryItem />
-                <RepositoryItem />
-                <RepositoryItem />
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </Container>
     )
