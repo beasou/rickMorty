121f3b0f38b8a35d77d9c6bce37f9ecfaf16f034
beasou <beatrizreginasouza1@gmail.com>
2022-06-29 18:04:09 -0300

    Estilizando lista de dados

--- rick-morty/src/components/RepositoryList/style.ts
+++ rick-morty/src/components/RepositoryList/style.ts
@@ -1,7 +1,32 @@
 import styled from 'styled-components'
 
 export const Container = styled.div `
-    img{
-        width: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    ul{
+        width: 100%;
+        display: flex ;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+        
+        li{
+            width: 300px;
+            margin: 20px;
+            list-style: none;
+        }
+        img{
+            width: 100%;
+            border-radius: .5rem;
+        }
+
+        h2{
+            width: 100%;
+            margin-top: 10px;
+        }
     }
+    
+   
 `
